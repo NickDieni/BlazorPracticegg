ad9770374cf7d1e75091d2ac0099408ad14db7e4
NickDieni <nickevereye@gmail.com>
2023-10-02 15:52:32 +0200

    Added a chat, still need to create the table on the sql
    CREATE TABLE ChatMessages
    (
        MessageID INT PRIMARY KEY IDENTITY(1,1),
        Content NVARCHAR(MAX) NOT NULL,
        Timestamp DATETIME NOT NULL
    );

---
+++ BlazorPractice/Data/DataChat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BlazorPractice.Data
+{
+    public class ChatMessage
+    {
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
