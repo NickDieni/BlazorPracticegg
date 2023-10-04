757f5de6ddbb68419e97b562680e98d66d4a02d0
NickDieni <nickevereye@gmail.com>
2023-10-04 12:54:16 +0200

    Made chat load chat and update/send chat to and from database

--- BlazorPractice/Data/DataChat.cs
+++ BlazorPractice/Data/DataChat.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorPractice.Data
 {
     public class ChatMessage
     {
+        [Key]
+        public int MessageID { get; set; }
         public string Content { get; set; }
         public DateTime Timestamp { get; set; }
+    }
+
+    public class ChatDbContext : DbContext
+    {
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options)
+        {
+        }
     }
 }
