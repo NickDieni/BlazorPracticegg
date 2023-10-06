8ce7fd379c4ccde98e5e374fb3c879f2faf667bf
NickDieni <nickevereye@gmail.com>
2023-10-06 11:58:10 +0200

    Added a rock paper scissors game, updated nav-menu with new icons and added a new selection to choose from.

---
+++ BlazorPractice/Data/RPSdata.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BlazorPractice.Data
+{
+    public class RPSdata
+    {
+        public string ComputerChoice { get; private set; }
+        public string GameResult { get; private set; }
+
+        private Random random = new Random();
+
+        public void PlayGame(string choice)
+        {
+            // Generate computer's choice randomly
+            int computerRandom = random.Next(1, 4);
+
+            // Map the random number to Rock, Paper, or Scissors
+            switch (computerRandom)
+            {
+                case 1:
+                    ComputerChoice = "Rock";
+                    break;
+                case 2:
+                    ComputerChoice = "Paper";
+                    break;
+                case 3:
+                    ComputerChoice = "Scissors";
+                    break;
+            }
+
+            // Determine the game result
+            if (choice == ComputerChoice)
+            {
+                GameResult = "It's a tie!";
+            }
+            else if ((choice == "Rock" && ComputerChoice == "Scissors") ||
+                     (choice == "Paper" && ComputerChoice == "Rock") ||
+                     (choice == "Scissors" && ComputerChoice == "Paper"))
+            {
+                GameResult = "You win!";
+            }
+            else
+            {
+                GameResult = "Computer wins!";
+            }
+        }
+    }
+}
